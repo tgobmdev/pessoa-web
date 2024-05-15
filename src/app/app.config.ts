066dea319d900958b9b2c0f64b73d7b4bfaf819d
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2024-05-14 22:04:14 -0300

    feat: add ng zorro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig } from '@angular/core'
+import { ApplicationConfig, importProvidersFrom } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
+import { provideNzIcons } from './icons-provider'
+import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n'
+import { registerLocaleData } from '@angular/common'
+import pt from '@angular/common/locales/pt'
+import { FormsModule } from '@angular/forms'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { provideHttpClient } from '@angular/common/http'
+
+registerLocaleData(pt)
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes)],
+    providers: [
+        provideRouter(routes),
+        provideNzIcons(),
+        provideNzI18n(pt_BR),
+        importProvidersFrom(FormsModule),
+        provideAnimationsAsync(),
+        provideHttpClient(),
+    ],
 }
