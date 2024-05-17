0b0bcacd3973b82ce5ea2eab95bac1ba29f7ad98
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2024-05-17 02:04:41 -0300

    feat: add base service

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, TemplateRef } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { Column } from './models/column.interface'
 
 @Component({
@@ -6,7 +6,6 @@ import { Column } from './models/column.interface'
     templateUrl: './table.component.html',
 })
 export class TableComponent<T extends Record<string, any>> {
-    @Input() title?: string | TemplateRef<void>
     @Input() dataSet!: T[]
     @Input() columns!: Column[]
 }
