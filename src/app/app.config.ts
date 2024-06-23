0af475bdbb5e1721da024a3d427c7445a28e1b42
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2024-06-23 14:53:47 -0300

    feat: adicionado interceptor para api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
 import { registerLocaleData } from '@angular/common'
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import pt from '@angular/common/locales/pt'
 import { FormsModule } from '@angular/forms'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { apiInterceptor } from '@core/interceptors/api.interceptor'
 import { provideNzI18n, pt_BR } from 'ng-zorro-antd/i18n'
 import { routes } from './app.routes'
 import { provideNzIcons } from './icons-provider'
@@ -19,6 +20,6 @@ export const appConfig: ApplicationConfig = {
 		provideNzI18n(pt_BR),
 		importProvidersFrom(FormsModule),
 		provideAnimationsAsync(),
-		provideHttpClient(),
+		provideHttpClient(withInterceptors([apiInterceptor])),
 	],
 }
